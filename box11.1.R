feba965e25364f8a4722b48779a2ca70dde3fa0f
Kevin Middleton <kmm@csusb.edu>
2012-03-08 20:24:25 -0800

    Update box 11.1

--- box11.1.R
+++ box11.1.R
@@ -1,10 +1,17 @@
 ## box11.1
 ## Data from Powick (1925)
-library(car)
+
+options(contrasts = c("contr.helmert", "contr.poly"))
 
 Lard <- read.csv("box11.1.csv")
 str(Lard)
 
+## Summarize
+library(plyr)
+ddply(Lard, .(Sex, Lard), 
+      function(x) c(mean = round(mean(x$Consumption), 2),
+                    sd = round(sd(x$Consumption), 2),
+                    n = length(x$Consumption)))
+
 fm <- lm(Consumption ~ Sex * Lard, data = Lard)
 anova(fm)
-Anova(fm, type = "III")
